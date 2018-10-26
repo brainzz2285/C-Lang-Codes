7e184646eea28fc88115f9e6cdf7793befbdedc4
brainzz <44133558+brainzz2285@users.noreply.github.com>
2018-10-26 17:25:50 +0530

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,29 @@
+/*19. Write a program to read a 2D array marks which stores the marks of five students in three
+subjects. Write a program to display the highest marks in each subject.*/
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int marks[5][3], i, j, max_marks;
+	for(i=0; i<5; i++)
+	{
+		printf("\n Enter the marks obtained by student %d",i+1);
+		for(j=0; j<3; j++)
+		{
+			printf("\n marks[%d][%d] = ", i, j);
+			scanf("%d", &marks[i][j]);
+		}
+	}
+	for(j=0; j<3; j++)
+	{
+		max_marks = –999;
+		for(i=0; i<5; i++)
+		{
+			if(marks[i][j]>max_marks)
+				max_marks = marks[i][j];
+		}
+	printf("\n The highest marks obtained in the subject %d = %d", j+1, max_marks);
+	}
+	getch();
+	return 0;
+}
